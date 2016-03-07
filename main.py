39b3266dc90ddf81848507a170e4d72aa09ea38b
Eugene Bolotin <eugenebolotin@MacBook-Pro-Eugene.local>
2016-03-07 20:48:31 +0300

    UI added

--- main.py
+++ main.py
@@ -3,23 +3,37 @@
 import tornado.ioloop
 import tornado.web
 from tornroutes import route
+from jinja2 import Environment, FileSystemLoader
 
-from NameGenerator import *
+from NameGenerator import NameGenerator
+
+
+templates = Environment(loader=FileSystemLoader('templates'))
 
 
 @route(r"/")
-class MainHandler(tornado.web.RequestHandler):
+class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("INDEX")
+        self.write(templates.get_template('index.html').render())
+
+
+@route(r"/shorten_url") 
+class ShortenUrlHandler(tornado.web.RequestHandler):
+    def post(self):
+        return self.write("OK")
 
 
 @route(r"/([\w+\-]+)") 
-class FollowLink(tornado.web.RequestHandler):
+class FollowLinkHandler(tornado.web.RequestHandler):
     def get(self, words):
         self.write("FOLLOW: " + words)
 
 
 if __name__ == "__main__":
+    routes = route.get_routes()
+    routes.append((r"/static/(.*)", tornado.web.StaticFileHandler, 
+                {"path": "static"}))
+
     app = tornado.web.Application(route.get_routes())
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
